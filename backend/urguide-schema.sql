b694f8c773e9eb43d98d17b555110cb7069c75e5
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-09-15 16:47:27 -0700

    Added extra tables but will narrow down usuable tables soon

--- backend/urguide-schema.sql
+++ backend/urguide-schema.sql
@@ -1,12 +1,11 @@
-
+// new user registers and is added to the database
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	first_name TEXT NOT NULL,
 	last_name TEXT NOT NULL,
 	username TEXT NOT NULL,
 	password TEXT NOT NULL,
-	email TEXT NOT NULL
-	  CHECK (position('@' IN email) > 1),
+	email TEXT NOT NULL,
 	phone INTEGER NOT NULL,
 	city TEXT NOT NULL,
 	country TEXT NOT NULL,
@@ -18,62 +17,142 @@ CREATE TABLE users (
 	interests TEXT
 );
 
+// coverts user to guide 
+CREATE TABLE guide (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(id),
+	is_guide BOOLEAN NOT NULL
+);
+
+// coverts user to tourist
+CREATE TABLE tourist (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(id),
+	is_tourist BOOLEAN NOT NULL
+);
+
+//  table that stores user guide 
+CREATE TABLE user_guide (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(id),
+	guide_id INTEGER REFERENCES guide(id)
+);
+
+// table that stores user tourist
+CREATE TABLE user_tourist (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(id),
+	tourist_id INTEGER REFERENCES tourist(id)
+);
 
 
+
+
+
+// user chooses to be a guide or tourist
 CREATE TABLE user_type (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	user_id INTEGER REFERENCES users(id),
-	user_type VARCHAR(50)
-);
+	type TEXT NOT NULL
+); 
 
-
-CREATE TABLE guides (
-	id serial PRIMARY KEY,
+//table that matches users with their interests
+CREATE TABLE user_interests (
+	id SERIAL PRIMARY KEY,
 	user_id INTEGER REFERENCES users(id),
-	guide_type VARCHAR(50)
+	interest TEXT NOT NULL
 );
 
-
-CREATE TABLE tourists (
-	id serial PRIMARY KEY,
+//table that matches users with their hobbies
+CREATE TABLE user_hobbies (
+	id SERIAL PRIMARY KEY,
 	user_id INTEGER REFERENCES users(id),
-	tourist_type VARCHAR(50)
+	hobby TEXT NOT NULL
 );
 
 
-CREATE TABLE likes (
-	id serial PRIMARY KEY,
+// table that matches users with their languages
+CREATE TABLE user_languages (
+	id SERIAL PRIMARY KEY,
 	user_id INTEGER REFERENCES users(id),
-	guide_id INTEGER REFERENCES guides(id)
+	language TEXT NOT NULL
 );
 
-
-CREATE TABLE dislikes (
-	id serial PRIMARY KEY,
+//table that matches users based on their locations 
+CREATE TABLE user_locations (
+	id SERIAL PRIMARY KEY,
 	user_id INTEGER REFERENCES users(id),
-	guide_id INTEGER REFERENCES guides(id)
+	city TEXT NOT NULL,
+	country TEXT NOT NULL,
+	zipCode INTEGER
 );
 
-
-CREATE TABLE likes_tourists (
-	id serial PRIMARY KEY,
+// user like table
+CREATE TABLE user_likes (
+	id SERIAL PRIMARY KEY,
 	user_id INTEGER REFERENCES users(id),
-	tourist_id INTEGER REFERENCES tourists(id)
+	liked_user_id INTEGER REFERENCES users(id)
 );
 
-
-CREATE TABLE dislikes_tourists (
-	id serial PRIMARY KEY,
+// user dislike table
+CREATE TABLE user_dislikes (
+	id SERIAL PRIMARY KEY,
 	user_id INTEGER REFERENCES users(id),
-	tourist_id INTEGER REFERENCES tourists(id)
+	disliked_user_id INTEGER REFERENCES users(id)
 );
 
 
-CREATE TABLE guide_tourist_match (
-	id serial PRIMARY KEY,
-	guide_id INTEGER REFERENCES guides(id),
-	tourist_id INTEGER REFERENCES tourists(id)
+
+// distance between two users is calculated
+CREATE TABLE user_distance (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(id),
+	distance INTEGER NOT NULL
 );
+
+// create a table where user tourist can rate a guide
+CREATE TABLE user_rating (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(id),
+	rating INTEGER NOT NULL
+);
+
+// tourist rate a guide
+CREATE TABLE ratings (
+	id SERIAL PRIMARY KEY,
+	tourist_id INTEGER REFERENCES users(id),
+	guide_id INTEGER REFERENCES users(id),
+	rating INTEGER NOT NULL
+);
+
+// guide can rate a tourist
+CREATE TABLE guide_ratings (
+	id SERIAL PRIMARY KEY,
+	tourist_id INTEGER REFERENCES users(id),
+	guide_id INTEGER REFERENCES users(id),
+	rating INTEGER NOT NULL
+);
+
+
+// user can leave a review for a guide
+CREATE TABLE reviews (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(id),
+	guide_id INTEGER REFERENCES users(id),
+	rating INTEGER NOT NULL,
+	review TEXT NOT NULL
+); 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
