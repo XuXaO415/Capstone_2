b3de2cb7c6f85e5d96444783486691b55ba1311b
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-10-07 17:24:14 -0700

    Added routes and auth

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,13 @@ const {
     NotFoundError
 } = require("./expressError");
 
+const {
+    authenticateJWT
+} = require("./middleware/auth");
+const authRoutes = require("./routes/auth");
+const usersRoutes = require("./routes/users");
+
+
 const morgan = require("morgan");
 
 const app = express();
@@ -14,6 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(authenticateJWT)
 
 //Handles 404 errors
 app.use(function (req, res, next) {
