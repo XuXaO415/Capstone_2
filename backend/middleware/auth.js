181fc516e720d4cdba9f7bf4c6b91ad7db991f6c
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-10-16 21:34:42 -0700

    Added middleware to handle auth cases

---
+++ backend/middleware/auth.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/** Convenience middleware to handle common auth cases in routes. */
+
+const jwt = require("jsonwebtoken");
+const {
+    SECRET_KEY
+} = require("../config");
+const {
+    UnauthorizedError
+} = require("../expressError");
+
+function authenticateJWT(req, res, next) {
+    try {
+        const authHeader = req.headers && req.headers.authorization;
+        if (authHeader) {
+            const token = authHeader.replace(/^[Bb]earer /, "").trim();
+            res.locals.user = jwt.verify(token, SECRET_KEY);
+        }
+        return next();
+    } catch (err) {
+        return next();
+    }
+}
+
+function ensureLoggedIn(req, res, next) {
+    try {
+        if (!res.locals.user) throw new UnauthorizedError();
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+}
+
+function ensureCorrectUser(req, res, next) {
+    try {
+        const user = res.locals.user;
+        if (!user || user.username !== req.params.username) {
+            throw new UnauthorizedError();
+        }
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+}
+
+module.exports = {
+    authenticateJWT,
+    ensureLoggedIn,
+    ensureCorrectUser,
+};
