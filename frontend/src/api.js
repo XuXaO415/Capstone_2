b342e630f1d56f1e37c5874bd0c9ad7aad2ec5ff
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-10-16 19:28:11 -0700

    Working on API helper

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -39,14 +39,20 @@ class UrGuideApi {
             throw Array.isArray(message) ? message : [message];
         }
     }
-
-    // Individual API calls
-    /* ************************************************************* */
-
-
-    // Temporary API token 
-
 }
+
+// Individual API calls
+/* ************************************************************* */
+
+
+// Temporary API token for testing -- CHANGE THIS LATER ONCE EVERYTHING IS WORKING!
+UrGuideApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
+    "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
+    "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
+
+
+
+
 
 
 export default UrGuideApi;
