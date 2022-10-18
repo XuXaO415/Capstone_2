529637d64031d8e80e59402defff3ed39691d407
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-10-17 17:16:37 -0700

    Added backend auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -44,8 +44,26 @@ function ensureCorrectUser(req, res, next) {
     }
 }
 
+/** Middleware to use when they be logged in as an admin user.
+ *
+ *  If not, raises Unauthorized.
+ */
+
+function ensureAdmin(req, res, next) {
+    try {
+        if (!res.locals.user || !res.locals.user.isAdmin) {
+            throw new UnauthorizedError();
+        }
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+}
+
+
 module.exports = {
     authenticateJWT,
     ensureLoggedIn,
     ensureCorrectUser,
+    ensureAdmin,
 };
