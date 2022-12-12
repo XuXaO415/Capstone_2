2f0608931cd8a172f7f4be91ba8eb0fc058e9f34
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-12-11 21:09:47 -0800

    Added form setup

---
+++ frontend/src/forms/ProfileForm.jsx
@@ -0,0 +1,4 @@
+// import * as React from 'react';
+
+// //Create and validate a form for user profile
+// function ProfileForm({ formData, setFormData, formDataErrors, setFormDataErrors, saveConfirmed, setSaveConfirmed, formErrors, setFormErrors, loading, setLoading, notFound, setNotFound, currentUser, setCurrentUser }) {
