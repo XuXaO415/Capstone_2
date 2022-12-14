ae3f2cc8bc3e207f46e0d8cffacc39939b9e11b2
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-12-13 20:01:43 -0800

    Omg, accidentally deleted homepage folder. Had to start from scratch

---
+++ frontend/src/Homepage.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useState, useEffect } from "react";
+import { Link, Route } from "react-router-dom";
+import UserContext from "./context/UserContext";
+import "./Homepage.css";
+
+function Homepage() {
+  const { currentUser } = useContext(UserContext);
+
+  console.debug("Homepage", "currentUser=", currentUser);
+
+  return (
+    <div className="Homepage App-header mt-5">
+      <div className="container text-center">
+        <h1 className="h1-welcome">Welcome to UrGuide</h1>
+        <p className="lead-paragraph">Your Friends are your best guides.</p>
+        <p className="second lead-paragraph">
+          UrGuide is a platform for you to share your favorite places.
+        </p>
+        {currentUser ? (
+          <h2>Welcome back, {currentUser.username}</h2>
+        ) : (
+          <center>
+            <Link to="/login" className="btn btn-primary mr-3">
+              Log in
+            </Link>
+            <Link to="/signup" className="btn btn-primary">
+              Sign up
+            </Link>
+          </center>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
