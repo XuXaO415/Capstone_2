e74d37c0be09a5e9c21640e99c3e4576f90fa77d
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-12-18 20:54:55 -0800

    Trying to clean up buttons/links for login and signup

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import UserContext from "./context/UserContext";
 import "./Homepage.css";
-import { FontAwesome } from "@fortawesome/react-fontawesome";
+import { Button } from "reactstrap";
 
 function Homepage() {
   const { currentUser } = useContext(UserContext);
@@ -20,18 +20,17 @@ function Homepage() {
             Welcome back, {currentUser.firstName || currentUser.username}!
           </h2>
         ) : (
-          <p>
-            <Link className="btn btn-primary font-weight-bold mr-3" to="/login">
-              Log in
-            </Link>
-            <p className="second-paragraph">Your guide to the world.</p>
-            <Link
-              className="btn btn-primary font-weight-bold mr-3"
-              to="/signup"
+          <div>
+            <Button
+              color="primary"
+              outline
+              href="/login"
+              className="btn btn-lg font-weight-bold mr-3"
+              value="Login"
             >
-              Sign up
-            </Link>
-          </p>
+              Login
+            </Button>
+          </div>
         )}
       </div>
     </div>
