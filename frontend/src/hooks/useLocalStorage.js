77a6dba9cb66ff5a36b16e10e3b18ada52a9c3ce
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-12-19 22:07:16 -0800

    modiefied local storage

--- frontend/src/hooks/useLocalStorage.js
+++ frontend/src/hooks/useLocalStorage.js
@@ -1,22 +1,23 @@
-import React, {
-    useState,
-    useEffect
-} from "react";
+import React, { useState } from "react";
 
 function useLocalStorage(key, initialValue = null) {
-    const firstValue = localStorage.getItem(key) || initialValue;
-    const [storedValue, setStoredValue] = useState(firstValue);
+  const firstValue = localStorage.getItem(key) || initialValue;
+  const [storedValue, setStoredValue] = useState(firstValue);
 
-    React.useEffect(() => {
-        localStorage.setItem(key, storedValue);
-        console.debug("Hook is running & using 'useLocalStorage', useEffect", "storedValue=", storedValue);
-        if (storedValue === null) {
-            localStorage.removeItem(key);
-        } else {
-            localStorage.setItem(key, storedValue);
-        }
-    }, [key, storedValue]);
-    return [storedValue, setStoredValue];
+  React.useEffect(() => {
+    localStorage.setItem(key, storedValue);
+    console.debug(
+      "Hook is running & using 'useLocalStorage', useEffect",
+      "storedValue=",
+      storedValue
+    );
+    if (storedValue === null) {
+      localStorage.removeItem(key);
+    } else {
+      localStorage.setItem(key, storedValue);
+    }
+  }, [key, storedValue]);
+  return [storedValue, setStoredValue];
 }
 
 export default useLocalStorage;
