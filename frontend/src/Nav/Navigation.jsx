2f645963cf2b896de4f94021a8e2b90e9d7e935a
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-01-05 22:40:28 -0800

    Fixed navbar

--- frontend/src/Nav/Navigation.jsx
+++ frontend/src/Nav/Navigation.jsx
@@ -1,31 +1,17 @@
 import React, { useContext, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink, Link, useHistory } from "react-router-dom";
 import UserContext from "../context/UserContext";
-import ProfileForm from "../forms/ProfileForm";
-import PrivateRoute from "../routes/PrivateRoute";
-import useLocalStorage from "../hooks/useLocalStorage";
-import UrGuideApi from "../api";
-import SignupForm from "../forms/SignupForm";
-import LoginForm from "../forms/LoginForm";
-import Homepage from "../Homepage";
-import {
-  Navbar,
-  Nav,
-  Container,
-  NavDropdown,
-  NavbarBrand,
-} from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-import "./Navigation.css";
+/** Navigation/Navbar component  */
 
 function Navigation({ logout }) {
   const { currentUser } = useContext(UserContext);
-  const [token, setToken] = useLocalStorage("token");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const history = useHistory();
 
-  console.debug("Navigation", "isLoggedIn=", isLoggedIn, "token =", token);
-
-  function loggedInNav() {
+  const loggedInNav = () => {
     return (
       <Nav className="mr-auto">
         <Nav.Link href="/" onClick={logout}>
@@ -33,83 +19,41 @@ function Navigation({ logout }) {
         </Nav.Link>
       </Nav>
     );
-  }
+  };
 
   const loggedOutNav = () => {
     return (
-      <NavbarBrand>
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item mr-4">
-            <NavLink className="nav-link" to="/login">
-              Login
-            </NavLink>
-          </li>
-          <li className="nav-item mr-4">
-            <NavLink className="nav-link" to="/signup">
-              Sign Up
-            </NavLink>
-          </li>
-        </ul>
-      </NavbarBrand>
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item mr-4">
+          <NavLink className="nav-link" to="/login">
+            Login
+          </NavLink>
+        </li>
+        <li className="nav-item mr-4">
+          <NavLink className="nav-link" to="/signup">
+            Sign Up
+          </NavLink>
+        </li>
+      </ul>
     );
   };
 
   return (
-    <nav className="Navigation navbar navbar-dark bg-light">
-      <NavLink className="navbar-brand" to="/">
-        UrGuide
-      </NavLink>
-      <Navbar.Collapse id="basic-navbar-nav">
-        {currentUser ? loggedInNav() : loggedOutNav()}
-      </Navbar.Collapse>
-    </nav>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">UrGuide</Navbar.Brand>
+        <img
+          alt=""
+          className="d-inline-block align-left"
+          src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/30/null/external-planet-protest-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"
+        />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {currentUser ? loggedInNav() : loggedOutNav()}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
-
-// function Navigation({ logout }) {
-//   const { currentUser } = useContext(UserContext);
-//   const [token, setToken] = useLocalStorage("token");
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-//   console.debug("Navigation", "isLoggedIn=", isLoggedIn, "token =", token);
-
-//   function loggedInNav() {
-//     return (
-//       <Nav className="mr-auto">
-//         <Nav.Link href="/" onClick={logout}>
-//           Log out {currentUser.first_name || currentUser.username}
-//         </Nav.Link>
-//       </Nav>
-//     );
-//   }
-
-//   const loggedOutNav = () => {
-//     return (
-//       <ul className="navbar-nav ml-auto">
-//         <li className="nav-item mr-4">
-//           <NavLink className="nav-link" to="/login">
-//             Login
-//           </NavLink>
-//         </li>
-//         <li className="nav-item mr-4">
-//           <NavLink className="nav-link" to="/signup">
-//             Sign Up
-//           </NavLink>
-//         </li>
-//       </ul>
-//     );
-//   };
-
-//   return (
-//     <nav className="Navigation navbar navbar-dark bg-light">
-//       <NavLink className="navbar-brand" to="/">
-//         UrGuide
-//       </NavLink>
-//       <Navbar.Collapse id="basic-navbar-nav">
-//         {currentUser ? loggedInNav() : loggedOutNav()}
-//       </Navbar.Collapse>
-//     </nav>
-//   );
-// }
 
 export default Navigation;
