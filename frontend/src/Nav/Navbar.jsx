e240802b82cdcde644a954c76f8f1cd6c68089e6
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-01-06 19:58:10 -0800

    Removed Navbar and combined it into Navigation component

--- frontend/src/Nav/Navbar.jsx
+++
@@ -1,5 +0,0 @@
-// import React from "react";
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import Homepage from "./Homepage";
-
-//This is a work in progress, but I hope to create a beautiful and functional navbar once I get the other components working :/
