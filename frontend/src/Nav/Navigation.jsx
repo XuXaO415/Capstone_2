faa2f3dfa9d033e73b2f2c0fb04b7c502c8a514f
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-01-13 20:14:07 -0800

    Revisit this section later

--- frontend/src/Nav/Navigation.jsx
+++ frontend/src/Nav/Navigation.jsx
@@ -5,30 +5,16 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-/** Navigation/Navbar component
- *
- * Props: login function, signup function, logout function
- * State: none
- * Context: currentUser
- * Routes -> Navigation
- *
- */
-
+/** Navigation/Navbar component */
 function Navigation({ logout }) {
+  const [currentUser, setCurrentUser] = useState(false);
   const history = useHistory();
-  const { currentUser } = useContext(UserContext);
-
-  function handleClick() {
-    logout();
-    history.push("/");
-  }
 
   const loggedInNav = () => {
     return (
       <Nav className="mr-auto">
-        <Nav.Link href="/profile">Profile</Nav.Link>
-        <Nav.Link href="/" onClick={handleClick}>
-          Log out {currentUser.firstName || currentUser.username}
+        <Nav.Link href="/" onClick={logout}>
+          Log out {currentUser.first_name || currentUser.username}
         </Nav.Link>
       </Nav>
     );
@@ -68,5 +54,60 @@ function Navigation({ logout }) {
     </Navbar>
   );
 }
+
+// function Navigation({ logout }) {
+//   const history = useHistory();
+//   const { currentUser } = useContext(UserContext);
+
+//   function handleClick() {
+//     logout();
+//     history.push("/");
+//   }
+
+//   const loggedInNav = () => {
+//     return (
+//       <Nav className="mr-auto">
+//         <Nav.Link href="/profile">Profile</Nav.Link>
+//         <Nav.Link href="/" onClick={handleClick}>
+//           Log out {currentUser.firstName || currentUser.username}
+//         </Nav.Link>
+//       </Nav>
+//     );
+//   };
+
+//   const loggedOutNav = () => {
+//     return (
+//       <ul className="navbar-nav ml-auto">
+//         <li className="nav-item mr-4">
+//           <NavLink className="nav-link" to="/login">
+//             Login
+//           </NavLink>
+//         </li>
+//         <li className="nav-item mr-4">
+//           <NavLink className="nav-link" to="/signup">
+//             Sign Up
+//           </NavLink>
+//         </li>
+//       </ul>
+//     );
+//   };
+
+//   return (
+//     <Navbar bg="dark" variant="dark" expand="lg">
+//       <Container>
+//         <Navbar.Brand href="/">UrGuide</Navbar.Brand>
+//         <img
+//           alt=""
+//           className="d-inline-block align-left"
+//           src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/30/null/external-planet-protest-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"
+//         />
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           {currentUser ? loggedInNav() : loggedOutNav()}
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// }
 
 export default Navigation;
