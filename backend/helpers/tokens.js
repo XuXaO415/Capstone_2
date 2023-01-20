71b53a4fa59761c3812f3ce2ff8fa9dce6ff10c8
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-01-19 21:41:53 -0800

    Made a tiny change

--- backend/helpers/tokens.js
+++ backend/helpers/tokens.js
@@ -1,20 +1,20 @@
 const jwt = require("jsonwebtoken");
-const {
-    SECRET_KEY
-} = require("../config");
+const { SECRET_KEY } = require("../config");
 
 function createToken(user) {
-    console.assert(user.isAdmin !== undefined,
-        "createToken passed user without isAdmin property");
+  console.assert(
+    user.isAdmin !== undefined,
+    "createToken passed user without isAdmin property"
+  );
 
-    let payload = {
-        username: user.username,
-        isAdmin: user.isAdmin || false,
-    };
+  let payload = {
+    username: user.username,
+    isAdmin: user.isAdmin || false,
+  };
 
-    return jwt.sign(payload, SECRET_KEY);
+  return jwt.sign(payload, SECRET_KEY);
 }
 
 module.exports = {
-    createToken
+  createToken,
 };
