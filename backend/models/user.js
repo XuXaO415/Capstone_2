986a09ff9ee28e124ba46c9375a01e5b93f86469
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-01-24 22:08:38 -0800

    Removed option for adding a user's home state

--- backend/models/user.js
+++ backend/models/user.js
@@ -67,7 +67,7 @@ class User {
 
   /** Register user with data.
    *
-   * Returns { username, first_name, last_name, phone, city, state, country, zipCode, latitude, longitude, image_url, hobbies, interests, is_admin }
+   * Returns { username, first_name, last_name, phone, city, country, zipCode, latitude, longitude, image_url, hobbies, interests, is_admin }
    *
    * Throws BadRequestError on duplicates.
    */
@@ -79,7 +79,6 @@ class User {
     lastName,
     email,
     city,
-    state,
     country,
     zipCode,
     latitude,
@@ -110,7 +109,6 @@ class User {
             last_name,
             email,
             city,
-            state,
             country,
             zip_code,
             latitude,
@@ -121,7 +119,7 @@ class User {
             is_admin
             )
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
-            RETURNING username, first_name AS "firstName", last_name AS "lastName", email, city, state, country, zip_code AS "zipCode", latitude, longitude, image_url AS "imageUrl", hobbies, interests, is_admin AS "isAdmin"`,
+            RETURNING username, first_name AS "firstName", last_name AS "lastName", email, city, country, zip_code AS "zipCode", latitude, longitude, image_url AS "imageUrl", hobbies, interests, is_admin AS "isAdmin"`,
       [
         username,
         hashedPassword,
@@ -129,7 +127,7 @@ class User {
         lastName,
         email,
         city,
-        state,
+
         country,
         zipCode,
         latitude,
@@ -147,9 +145,11 @@ class User {
 
   /** Find all users.
    *
-   * Returns [{ username, first_name, last_name, email, phone, city, state, country, zip_code, latitude, longitude, image_url, hobbies, interests, is_admin, is_guide, is_tourist }, ...]
+   * Returns [{ username, first_name, last_name, email, phone, city, country, zip_code, latitude, longitude, image_url, hobbies, interests, is_admin, is_guide, is_tourist }, ...]
    *
    * Throws NotFoundError if no users found.
+   *
+   * Left state in for now
    * */
 
   static async findAll() {
@@ -159,7 +159,6 @@ class User {
             last_name AS "lastName",
             email,
             city,
-            state,
             country,
             zip_code AS "zipCode",
             latitude,
