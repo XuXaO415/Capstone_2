99344bae19ba1f886eb8ff0788ec03a645b7e2aa
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-02-03 22:11:03 -0800

    Match detail card is now working though no matches are populated

--- frontend/src/matches/MatchDetail.jsx
+++ frontend/src/matches/MatchDetail.jsx
@@ -1,38 +1,126 @@
-import * as React from "react";
-import { Link, NavLink, Switch, Route, Redirect } from "react-router-dom";
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import UrGuideApi from "../api";
+import UserContext from "../context/UserContext";
+import { Card } from "react-bootstrap";
 
-function MatchDetail({ UserMatchInfo, currNum, totalNum }) {
-  const {
+/** Match Detail page
+ *
+ * Renders a potential match's profile
+ *
+ * Routed at users/username/matches/:user_id
+ *
+ * Routes -> MatchDetail -> MatchList
+ */
+
+function MatchDetail() {
+  const { username, user_id } = useParams();
+  const { currentUser } = useContext(UserContext);
+  const [matchInfo, setMatchInfo] = useState(null);
+
+  console.debug(
+    "MatchDetail",
+    "currentUser=",
+    currentUser,
+    "username=",
     username,
-    first_name,
-    last_name,
-    hobbies,
-    interests,
-    city,
-    country,
-    zipCode,
-    // latitude,
-    // longitude,
-    image_url,
-  } = UserMatchInfo;
+    "user_id=",
+    user_id
+  );
 
+  useEffect(
+    function getPotentialUserMatches() {
+      async function getPotentialMatches() {
+        setMatchInfo(await UrGuideApi.getPotentialMatches(username, user_id));
+      }
+      getPotentialMatches();
+    },
+    [username, user_id]
+  );
+
+  if (!matchInfo) return <p>Loading...</p>;
   return (
     <div className="MatchDetail">
-      <h1>
-        Here are some of your matches {currNum} of {totalNum}
-      </h1>
-      <h2>
-        {first_name} {last_name}
-      </h2>
-      <p className="MatchDetail-card-title">Username: {username}</p>
-      <p className="MatchDetail-card-title">My hobbies: {hobbies}</p>
-      <p className="MatchDetail-card-title">My interests: {interests}</p>
-      <p className="MatchDetail-card-title">
-        I'm from {city}, {country}, {zipCode}
-      </p>
-      <img className="MatchDetail-card-img" src={image_url} alt="user-img" />
+      <Card>
+        <h3 className="text-center">You matched with: {matchInfo.username}</h3>
+        <Card.Img variant="top" src={matchInfo.image_url} />
+        <Card.Body>
+          <Card.Title>
+            {matchInfo.first_name} {matchInfo.last_name}
+          </Card.Title>
+          <Card.Text>
+            <strong>City:</strong> {matchInfo.city}
+          </Card.Text>
+          <Card.Text>
+            <Card.Text>
+              <strong>State:</strong> {matchInfo.state}
+            </Card.Text>
+            <strong>Country:</strong> {matchInfo.country}
+          </Card.Text>
+          <Card.Text>
+            <strong>Zip Code:</strong> {matchInfo.zipCode}
+          </Card.Text>
+          <Card.Text>
+            <strong>Hobbies:</strong> {matchInfo.hobbies}
+          </Card.Text>
+          <Card.Text>
+            <strong>Interests:</strong> {matchInfo.interests}
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
+
+// function MatchDetail() {
+//   const { username, user_id } = useParams();
+//   const [matchInfo, setMatchInfo] = useState(null);
+
+//   console.debug("MatchDetail", "username=", username, "user_id=", user_id);
+
+//   useEffect(
+//     function getPotentialUserMatches() {
+//       async function getPotentialMatches() {
+//         setMatchInfo(await UrGuideApi.getPotentialMatches(username, user_id));
+//       }
+//       getPotentialMatches();
+//     },
+//     [username, user_id]
+//   );
+
+//   if (!matchInfo) return <p>Loading...</p>;
+
+//   return (
+//     <div className="MatchDetail">
+//       <Card>
+//         <h3 className="text-center">You matched with: {matchInfo.username}</h3>
+//         <Card.Img variant="top" src={matchInfo.image_url} />
+//         <Card.Body>
+//           <Card.Title>
+//             {matchInfo.first_name} {matchInfo.last_name}
+//           </Card.Title>
+//           <Card.Text>
+//             <strong>City:</strong> {matchInfo.city}
+//           </Card.Text>
+//           <Card.Text>
+//             <Card.Text>
+//               <strong>State:</strong> {matchInfo.state}
+//             </Card.Text>
+//             <strong>Country:</strong> {matchInfo.country}
+//           </Card.Text>
+//           <Card.Text>
+//             <strong>Zip Code:</strong> {matchInfo.zipCode}
+//           </Card.Text>
+//           <Card.Text>
+//             <strong>Hobbies:</strong> {matchInfo.hobbies}
+//           </Card.Text>
+//           <Card.Text>
+//             <strong>Interests:</strong> {matchInfo.interests}
+//           </Card.Text>
+//         </Card.Body>
+//       </Card>
+//     </div>
+//   );
+// }
 
 export default MatchDetail;
