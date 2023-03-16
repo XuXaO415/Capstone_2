4bd85441406a56aae30ce095804f9fb407bfa227
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-03-15 20:08:46 -0700

    Working on getting user info pill to route to the right user

--- frontend/src/matches/LikeMatchList.jsx
+++ frontend/src/matches/LikeMatchList.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Link, useHistory, Route } from "react-router-dom";
+import { Link, useHistory, Route, useParams } from "react-router-dom";
 import UrGuideApi from "../api";
 import UserContext from "../context/UserContext";
-import MatchDetail from "./MatchDetail";
 import MatchCard from "./MatchCard";
 
 /** This component retrieves a user's likes and displays it's list here.
@@ -16,30 +15,44 @@ import MatchCard from "./MatchCard";
 
 const LikeMatchList = () => {
   const { currentUser, user_id } = useContext(UserContext);
+  // const { user_id } = useParams();
   const [likedMatches, setLikedMatches] = useState(null);
-  const [error, setError] = useState(null);
+  const [setError] = useState(null);
 
   useEffect(() => {
     (async () => {
-      await UrGuideApi.getLikedMatches(currentUser.username, user_id).then(
-        (res) => setLikedMatches(res)
-      );
+      try {
+        const res = await UrGuideApi.getLikedMatches(
+          currentUser.username,
+          user_id
+        );
+        setLikedMatches(res);
+      } catch (error) {
+        console.error(error);
+      }
     })();
   }, [currentUser.username, user_id]);
 
+  // useEffect(() => {
+  //   (async () => {
+  //     await UrGuideApi.getLikedMatches(currentUser.username, user_id).then(
+  //       (res) => setLikedMatches(res)
+  //     );
+  //   })();
+  // }, [currentUser.username, user_id]);
+
   async function dislikeMatch(user_id) {
     try {
-      let matchInfo = UrGuideApi.dislikeMatch(currentUser.username, user_id);
       setLikedMatches((m) => m.filter((match) => match.user_id !== user_id));
-    } catch (errors) {
-      setError(errors);
+    } catch (err) {
+      setError(err);
     }
   }
 
   if (!likedMatches) return <p>Loading...</p>;
 
   return (
-    <div className="MatchList">
+    <div className="MatchList-header">
       <div className="text-center">
         <h1 className="mb-4">Liked Matches</h1>
       </div>
@@ -49,22 +62,21 @@ const LikeMatchList = () => {
           {likedMatches.map((l) => (
             <MatchCard
               key={l.id}
+              // key={l.user_id}
               user_id={l.user_id}
               username={l.username}
               first_name={l.first_name}
-              last_name={l.last_name}
               image_url={l.image_url}
               city={l.city}
               state={l.state}
+              country={l.country}
               zip_code={l.zip_code}
               interests={l.interests}
               hobbies={l.hobbies}
               dislike={dislikeMatch}
+              // dislike={(matchInfo) => dislikeMatch(matchInfo.user_id)}
             />
           ))}
-          {/* <Route exact path="/:users/:username/matches/likes/:user_id">
-            <MatchDetail />
-          </Route> */}
         </div>
       ) : (
         <p>You haven't liked any users yet.</p>
