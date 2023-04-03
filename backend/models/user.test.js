6d0888812062ce84364bf385e8efa243f6820040
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-04-03 16:01:15 -0700

    Cool, test are passing, tho still working on this section

--- backend/models/user.test.js
+++ backend/models/user.test.js
@@ -23,51 +23,61 @@ afterAll(commonAfterAll);
 
 describe("authenticate", function () {
   test("works", async function () {
-    const user = await User.authenticate("johndoe", "password");
+    const user = await User.authenticate("jdoe", "password");
     expect(user).toEqual({
-      username: "johndoe",
-      first_name: "John",
+      username: "jdoe",
+      first_name: "Jane",
       last_name: "Doe",
-      email: "test3@email.com",
+      email: "test@email.com",
       is_admin: false,
     });
   });
-
-  test("throws UnauthorizedError if invalid password", async function () {
-    expect.assertions(1);
-    try {
-      await User.authenticate("johndoe", "wrongpassword");
-    } catch (err) {
-      expect(err instanceof UnauthorizedError).toBeTruthy();
-    }
-  });
-
-  test("throws NotFoundError if user not found", async function () {
-    expect.assertions(1);
-    try {
-      await User.authenticate("nonexistentuser", "password");
-    } catch (err) {
-      expect(err instanceof NotFoundError).toBeTruthy();
-    }
-  });
 });
 
-describe("create", function () {
+test("throws UnauthorizedError if invalid password", async function () {
+  expect.assertions(1);
+  try {
+    await User.authenticate("nope", "wrongpassword");
+  } catch (err) {
+    expect(err instanceof UnauthorizedError).toBeTruthy();
+  }
+});
+
+test("throws UnauthorizedError if invalid username", async function () {
+  expect.assertions(1);
+  try {
+    await User.authenticate("_nope_", "password");
+  } catch (err) {
+    expect(err instanceof UnauthorizedError).toBeTruthy();
+  }
+});
+
+describe("register", function () {
   test("works", async function () {
-    const newUser = {
+    let user = await User.register({
       username: "newuser",
       password: "newpassword",
       first_name: "New",
       last_name: "User",
-      email: "newuser@example.com",
-      is_admin: false,
-    };
-    const user = await User.create(newUser);
+      email: "hello@gmail.com",
+    });
     expect(user).toEqual({
       username: "newuser",
       first_name: "New",
       last_name: "User",
-      email: "newuser@example.com",
+      email: "hello@gmail.com",
+      is_admin: false,
+    });
+
+    const found = await db.query(
+      "SELECT username, first_name, last_name, email, is_admin FROM users WHERE username = 'newuser'"
+    );
+    expect(found.rows.length).toEqual(1);
+    expect(found.rows[0]).toEqual({
+      username: "newuser",
+      first_name: "New",
+      last_name: "User",
+      email: "hello@gmail.com",
       is_admin: false,
     });
   });
@@ -131,8 +141,8 @@ describe("update", function () {
     try {
       await User.update("nonexistentuser", {
         first_name: "Jane",
-        last_name: "Doe",
-        email: "janedoe@example.com",
+        last_name: "Doeee",
+        email: "test@email.com",
       });
     } catch (err) {
       expect(err instanceof NotFoundError).toBeTruthy();
