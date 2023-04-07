7ffe38d25481b3e0753bcdf9462ce0fa8f0a9a57
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-04-06 19:48:14 -0700

    Altered testing setup

--- backend/models/_testCommon.js
+++ backend/models/_testCommon.js
@@ -3,20 +3,17 @@ const bcrypt = require("bcrypt");
 const db = require("../db.js");
 const { BCRYPT_WORK_FACTOR } = require("../config");
 
-/** Common setup for all tests; add data to db. */
-
 async function commonBeforeAll() {
   // noinspection SqlWithoutWhere
   await db.query("DELETE FROM users");
 
   await db.query(
-    `
-        INSERT INTO users (first_name, last_name, username, password, email, is_admin)
-        VALUES('Jane', 'Doe', 'jdoe', 'password', 'test@email.com', true),
-                ('John', 'Doe', 'johndoe', 'password2', 'test2@email.com', false))
+    `INSERT INTO users(username, password, first_name, last_name, email, city, state, zip_code, country, latitude, longitude, image_url, hobbies, interests)
+        VALUES('jdoe', $1, 'Jane', 'Doe', 'test@email.com', 'SF', 'CA', '94110', 'US', '000000', '000000', NULL,  NULL, NULL),
+              ('newuser', $2, 'New', 'User', 'test2@email.com', 'Oakland', 'CA', '94601', 'US', '100000', '100000', NULL,  NULL, NULL)
         RETURNING username`,
     [
-      await bcrypt.hash("password", BCRYPT_WORK_FACTOR),
+      await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
       await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
     ]
   );
