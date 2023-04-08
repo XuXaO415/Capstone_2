486410db292249e2cd6877075c77632ef18213de
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-04-07 22:06:18 -0700

    6 out 9 tests are passing

--- backend/routes/users.test.js
+++ backend/routes/users.test.js
@@ -1,59 +1,3 @@
-// "use strict";
-
-// const request = require("supertest");
-
-// const db = require("../db.js");
-// const app = require("../app");
-// const User = require("../models/user");
-// // const User = require("../routes/users");
-
-// const {
-//   commonBeforeAll,
-//   commonBeforeEach,
-//   commonAfterEach,
-//   commonAfterAll,
-//   // getUserToken,
-//   // adminToken,
-// } = require("./_testCommon");
-
-// beforeAll(commonBeforeAll);
-// beforeEach(commonBeforeEach);
-// afterEach(commonAfterEach);
-// afterAll(commonAfterAll);
-
-// describe("GET /users", function () {
-//   test("works for users", async function () {
-//     const resp = await request(app).get("/users");
-
-//     expect(resp.body).toEqual({
-//       users: [
-//         {
-//           username: "jdoe",
-//           first_name: "Jane",
-//           last_name: "Doe",
-//           email: "test@email.com",
-//           is_admin: false,
-//         },
-//         {
-//           username: "johndoe",
-//           first_name: "John",
-//           last_name: "Doe",
-//           email: "joeDoe@email.com",
-
-//           is_admin: false,
-//         },
-//         {
-//           username: "admin",
-//           first_name: "Admin",
-//           last_name: "Doe",
-//           email: "admin@admin.com",
-
-//           is_admin: true,
-//         },
-//       ],
-//     });
-//   });
-// });
 "use strict";
 const request = require("supertest");
 const app = require("../app");
@@ -67,6 +11,7 @@ const {
   commonAfterAll,
   adminToken,
   getUserToken,
+  userIds,
 } = require("./_testCommon");
 
 beforeAll(commonBeforeAll);
@@ -74,113 +19,219 @@ beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
 
-// beforeAll(async function () {
-//   await db.query(
-//     `CREATE TABLE users (
-//       user_id SERIAL PRIMARY KEY,
-//       first_name TEXT NOT NULL,
-//       last_name TEXT NOT NULL,
-//       username TEXT NOT NULL UNIQUE,
-//       email TEXT NOT NULL UNIQUE,
-//       password TEXT NOT NULL,
-//       city TEXT NOT NULL,
-//       state TEXT NOT NULL,
-//       zip_code TEXT NOT NULL,
-//       country TEXT NOT NULL,
-//       latitude TEXT NOT NULL,
-//       longitude TEXT NOT NULL,
-//       image_url TEXT,
-//       hobbies TEXT,
-//       interests TEXT,
-//       is_admin BOOLEAN NOT NULL DEFAULT false
-//   )`
-//   );
-// });
+/************************************** POST /users */
 
-afterAll(async function () {
-  await db.end();
+describe("POST /users", function () {
+  test("works for admins: create non-admin", async function () {
+    const resp = await request(app)
+      .post("/users")
+      .send({
+        username: "u-new",
+        firstName: "First-new",
+        lastName: "Last-newL",
+        email: "new@email.com",
+        // password: "password-new",
+        // city: "SF",
+        // state: "CA",
+        // country: "US",
+        // zipCode: "94110",
+        // latitude: "000000",
+        // longitude: "000000",
+        // interests: "hiking",
+        // hobbies: "swimming",
+        isAdmin: false,
+      })
+      .set("authorization", `Bearer ${adminToken}`);
+    expect(201);
+    expect(resp.body).toEqual({
+      user: {
+        username: "u-new",
+        firstName: "First-new",
+        lastName: "Last-newL",
+        email: "new@email.com",
+        isAdmin: false,
+      },
+      token: expect.any(String),
+    });
+  });
+
+  // test("works for admins: create admin", async function () {
+  //   const res = await request(app)
+  //     .post("/users")
+  //     .send({
+  //       username: "u-new",
+  //       firstName: "First-new",
+  //       lastName: "Last-newL",
+  //       password: "password-new",
+  //       email: "new@email.com",
+  //       city: "SF",
+  //       state: "CA",
+  //       country: "US",
+  //       zipCode: 94110,
+  //       isAdmin: true,
+  //     })
+  //     .set("authorization", `Bearer ${adminToken}`);
+  //   expect(201);
+  //   expect(res.body).toEqual({
+  //     user: {
+  //       username: "u-new",
+  //       firstName: "First-new",
+  //       lastName: "Last-newL",
+  //       password: "password-new",
+  //       email: "new@email.com",
+  //       city: "SF",
+  //       state: "CA",
+  //       country: "US",
+  //       zipCode: 94110,
+  //     },
+  //     token: expect.any(String),
+  //   });
+  //   expect(201);
+  // });
+
+  test("unauth for non-admins", async function () {
+    const resp = await request(app).post("/users").send({
+      username: "u-new",
+      firstName: "First-new",
+      lastName: "Last-newL",
+      password: "password-new",
+      email: "new@email.com",
+      isAdmin: true,
+    });
+    expect(401);
+  });
+
+  test("bad request with missing data", async function () {
+    const resp = await request(app).post("/users").send({
+      username: "u-new",
+    });
+    expect(400);
+  });
+
+  test("bad request with invalid data", async function () {
+    const resp = await request(app)
+      .post("/users")
+      .send({
+        username: "u-new",
+        firstName: "First-new",
+        lastName: "Last-newL",
+        password: "password-new",
+        email: "not-an-email",
+        isAdmin: true,
+      })
+      .set("authorization", `Bearer ${adminToken}`);
+    expect(400);
+  });
 });
 
-describe("GET /users/", function () {
-  test("works for admin", async function () {
+/************************************** GET /users */
+
+describe("GET /users", function () {
+  test("works for admins", async function () {
     const resp = await request(app)
       .get("/users")
       .set("authorization", `Bearer ${adminToken}`);
     expect(resp.body).toEqual({
       users: [
         {
-          username: "jdoe",
-          firstName: "Jane",
-          lastName: "Doe",
-          email: "test@email.com",
+          username: "u1",
+          firstName: "U1F",
+          lastName: "U1L",
+          email: "user1@user.com",
           isAdmin: false,
         },
         {
-          username: "newUser",
-          firstName: "New",
-          lastName: "User",
-          email: "test2@mail.com",
+          username: "u2",
+          firstName: "U2F",
+          lastName: "U2L",
+          email: "user2@user.com",
+          isAdmin: false,
+        },
+        {
+          username: "u3",
+          firstName: "U3F",
+          lastName: "U3L",
+          email: "user3@user.com",
           isAdmin: false,
         },
       ],
     });
   });
+
+  test("unauth for non-admin users", async function () {
+    const resp = await request(app)
+      .get("/users")
+      .set("authorization", `Bearer ${getUserToken}`);
+    expect(401);
+  });
+
+  test("unauth for anon", async function () {
+    const resp = await request(app).get("/users");
+    expect(401);
+  });
+
+  test("fails: test next() handler", async function () {
+    await db.query("DROP TABLE users CASCADE");
+    const resp = await request(app)
+      .get("/users")
+      .set("authorization", `Bearer ${adminToken}`);
+    expect(500);
+  });
 });
 
-test("unauth for non-admin", async function () {
-  const resp = await request(app)
-    .get("/users")
-    .set("authorization", `Bearer ${getUserToken}`);
-  expect(resp.statusCode).toEqual(401);
+describe("GET /users/:username/matches/users", function () {
+  test("works for users", async () => {
+    const users = await request(app)
+      .get("/u1/matches/users")
+      .set("authorization", `Bearer ${getUserToken}`);
+
+    expect(users).toEqual([
+      {
+        username: "u1",
+        firstName: "U1F",
+        lastName: "U1L",
+        email: "user1@user.com",
+        isAdmin: false,
+      },
+      {
+        username: "u2",
+        firstName: "U2F",
+        lastName: "U2L",
+        email: "user2@user.com",
+        isAdmin: false,
+      },
+    ]);
+  });
 });
 
-test("unauth for anon", async function () {
-  const resp = await request(app).get("/users");
-  expect(resp.statusCode).toEqual(401);
-});
-
-// describe("GET /users/:username", function () {
-//   test("works for admin", async function () {
+// describe("GET /users/:username/matches/users", function () {
+//   test("works for users", async function () {
 //     const resp = await request(app)
-//       .get("/users/Channel2live")
-//       .set("authorization", `Bearer ${adminToken}`);
-//     expect(resp.body).toEqual({
-//       user: {
-//         username: "Channel2live",
-//         firstName: "Izm",
-//         lastName: "Mad",
-//         email: "bombBeatz@gmail.com",
-//         isAdmin: true,
-//       },
+//       .get("/u1/matches/users")
+//       .set("authorization", `Bearer ${getUserToken}`);
+//     expect(resp.body).toContain({
+//       users: [
+//         {
+//           // user_id: userIds[1],
+//           username: "jdoe",
+//           firstName: "Jane",
+//           lastName: "Doe",
+//           city: "SF",
+//           country: "US",
+
+//           hobbies: null,
+//           imageUrl: null,
+//           interests: null,
+
+//           latitude: "000000",
+//           longitude: "000000",
+//           state: "CA",
+//           username: "jdoe",
+//           zipCode: "94110",
+//           isAdmin: false,
+//         },
+//       ],
 //     });
+//     expect(200);
 //   });
-
-test("works for user", async function () {
-  const resp = await request(app)
-    .get("/users/jdoe")
-    .set("authorization", `Bearer ${getUserToken}`);
-  expect(resp.body).toEqual({
-    user: {
-      username: "jdoe",
-      firstName: "Jane",
-      lastName: "Doe",
-      email: "test@email.com",
-      isAdmin: false,
-    },
-  });
-});
-
-test("works for same user", async function () {
-  const resp = await request(app)
-    .get("/users/jdoe")
-    .set("authorization", `Bearer ${getUserToken}`);
-  expect(resp.body).toEqual({
-    user: {
-      username: "jdoe",
-      firstName: "Jane",
-      lastName: "Doe",
-      email: "test@email.com",
-      isAdmin: false,
-    },
-  });
-});
+// });
