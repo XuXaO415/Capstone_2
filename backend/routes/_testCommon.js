3f0731c245165ecaa7a1f503a29d6c5d5a8d8475
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-04-09 22:57:01 -0700

    Removed unnecessary methods

--- backend/routes/_testCommon.js
+++ backend/routes/_testCommon.js
@@ -2,134 +2,52 @@
 
 const db = require("../db.js");
 const User = require("../models/user");
+// const User = require("../routes/users");
 const { createToken } = require("../helpers/tokens");
 
 const userIds = [];
-let getUserToken, adminToken;
+// let getUserToken, adminToken;
 
-const commonBeforeAll = async () => {
-  await db.query("DELETE FROM users");
+async function commonBeforeAll() {
+  // await db.query("DELETE FROM users");
+  // await db.query(
+  //   `INSERT INTO users(username, password, first_name, last_name, email, city, state, zip_code, country, latitude, longitude, image_url, hobbies, interests)
+  //       VALUES('jdoe', 'password', 'Jane', 'Doe', 'test@email.com', 'SF', 'CA', '94110', 'US', '000000', '000000', NULL,  NULL, NULL),
+  //             ('newuser', 'password2', 'New', 'User', 'test2@email.com', 'Oakland', 'CA', '94601', 'US', '100000', '100000', NULL,  NULL, NULL)
+  //       RETURNING username`
+  // );
+}
 
-  // await db.query("DELETE FROM likes");
-  // await db.query("DELETE FROM test_users");
-  // await db.query("DELETE FROM test_likes");
-
-  // await db.query(`
-  //   INSERT INTO users (username, password, first_name, last_name, email, is_admin)
-  //   VALUES ('testuser1', 'password', 'John', 'Doe', 'new@email.com', true),
-  //   ('testuser2', 'password', 'Jane', 'Doe', 'tester@mail.com', false);
-  // `);
-
-  // await db.query(`
-  //   INSERT INTO likes (user_id, liked_user, liked_username)
-  //   VALUES (1, 2, 'testuser2'),
-  //   (2, 1, 'testuser1');
-  // `);
-
-  // await db.query(`
-  //   INSERT INTO test_users (username, password, first_name, last_name, email, is_admin)
-  //   VALUES ('testuser1', 'password', 'John', 'Doe', 'johndoe@example.com', true),
-  //          ('testuser2', 'password', 'Jane', 'Doe', 'janedoe@example.com', false),
-  //          ('testuser3', 'password', 'Bob', 'Smith', 'bobsmith@example.com', false);
-  // `);
-
-  // await db.query(`
-  //   INSERT INTO test_likes (user_id, liked_user, liked_username)
-  //   VALUES (1, 2, 'testuser2'),
-  //          (2, 1, 'testuser1');
-  // `);
-
-  const createUser = await User.register({
-    username: "testuser",
-    first_name: "Test",
-    last_name: "User",
-    email: "test@email.com",
-    password: "password",
-    is_admin: false,
-  });
-  await User.register({
-    username: "testuser",
-    first_name: "Test",
-    last_name: "User",
-    email: "test@email.com",
-    password: "password",
-    is_admin: false,
-  });
-
-  userIds.push(createUser.id);
-
-  const result = await db.query(
-    `SELECT id, username, first_name, last_name, email, is_admin
-    FROM users
-    WHERE username = 'testuser'`
-  );
-
-  const user = result.rows[0];
-
-  getUserToken = createToken(user);
-};
-
-const commonBeforeEach = async () => {
+async function commonBeforeEach() {
   await db.query("BEGIN");
-};
+  // console.log("Test starting");
+}
 
-const commonAfterEach = async () => {
+async function commonAfterEach() {
   await db.query("ROLLBACK");
-};
+}
 
-const commonAfterAll = async () => {
+async function commonAfterAll() {
   await db.end();
-};
+}
 
-//   await User.register({
-//     username: "johndoe",
-//     first_name: "John",
-//     last_name: "Doe",
-//     email: "johnD@email.com",
-//     password: "password",
-//     is_admin: false,
-//   });
-
-//   await User.register({
-//     username: "janeDoe",
-//     first_name: "Jane",
-//     last_name: "Doe",
-//     email: "janeD@test.com",
-//     password: "password",
-//     is_admin: false,
-//   });
-
-//   await User.register({
-//     username: "admin",
-//     first_name: "Admin",
-//     last_name: "User",
-//     email: "adminUser@admin.com",
-//     password: "password1",
-//     is_admin: true,
-//   });
-
-//   getUserToken = createToken({ username: "testuser", is_admin: false });
-//   adminToken = createToken({ username: "admin", is_admin: true });
-// };
-
-// async function commonBeforeEach() {
-//   await db.query("BEGIN");
-// }
-
-// async function commonAfterEach() {
-//   await db.query("ROLLBACK");
-// }
-
-// async function commonAfterAll() {
-//   await db.end();
-// }
+const adminToken = createToken({
+  username: "admin",
+  password: "admin",
+  isAdmin: true,
+});
+const getUserToken = createToken({
+  username: "jdoe",
+  // password: "password",
+  isAdmin: false,
+});
 
 module.exports = {
   commonBeforeAll,
   commonAfterEach,
   commonAfterAll,
   commonBeforeEach,
-  userIds,
-  getUserToken,
   adminToken,
+  getUserToken,
+  userIds,
 };
